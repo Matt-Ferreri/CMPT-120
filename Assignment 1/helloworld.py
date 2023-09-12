0f546e99b5c7b3a9dccd4259132210ceeb321293
Matt-Ferreri <ma8ferreri@gmail.com>
2023-09-12 08:14:33 -0400

    HW 1, Hello World

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -3,14 +3,11 @@
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
 
-
 def main():
+    print("Hello World!")
+    color="red"
+    print("My favorite color is:",color)
+    birthday=982004
+    print("My birthday is:", birthday)
 
-
-
-    
-    
-    
-    
-    
 main()
